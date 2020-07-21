1d6f201098440b3b5a9aef1f4d08baebf6cbe9b4
Marc Robotham <forxnives@gmail.com>
2020-07-20 21:06:20 -0700

    features section and starting popular items section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './img/logo.jpg';
 import hero from './img/baskets_hero.jpg';
 import maskgirl from './img/maskgirl_hero.png'
+import woodspoon from './img/wood_spoons.jpg'
 
 
 function App() {
@@ -120,7 +121,6 @@ function App() {
             </ul>
           </div>
 
-
         </div>
 
       </section>
@@ -135,43 +135,190 @@ function App() {
 
           <div className="hero-grid-left">
 
-
-
             <div className="hero-grid-left__txt">
 
               <h2>SOMETHING FOR FUCKS SAKE</h2>
             </div>
 
-
             <img src={maskgirl} alt="baskets"/>
 
 
-
-
-
-
-
-            
           </div>
-
-
 
           <div className="hero-grid-right">
 
             <img src={hero} alt="" className="hero-grid-right-img__hide"/>
-
-
 
             <div className="hero-grid-right__txt">
             <h2 className="">Shop all things ready to ship</h2>
 
             </div>
 
-
-
           </div>
 
+        </div>
 
+
+      </section>
+
+      <section className="features">
+
+        <div className="features-container">
+
+         <div className="features-container-flex">
+
+            <div className="feature feature1">
+              
+              <h3 className="feature-heading">
+
+                <span className="feature-heading__icon">
+
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><path d="M9.057,20.471L2.293,13.707a1,1,0,0,1,1.414-1.414l5.236,5.236,11.3-13.18a1,1,0,1,1,1.518,1.3Z"></path></svg>
+
+
+                </span>
+
+                <span className="feature-heading__txt">
+                  One-of-a-kind
+
+
+                </span>
+
+
+
+              </h3>
+
+              <div className="feature-txt">
+
+                <span className="feature-txt__txt">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia 
+                  provident fuga veniam autem praesentium repellendus id exercitationem 
+                  dolorem saepe dolores aliquam odio, 
+                </span>
+
+              </div>
+
+            </div>
+
+            <div className="feature feature2">
+
+
+            <h3 className="feature-heading">
+
+                <span className="feature-heading__icon">
+
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><path d="M9.057,20.471L2.293,13.707a1,1,0,0,1,1.414-1.414l5.236,5.236,11.3-13.18a1,1,0,1,1,1.518,1.3Z"></path></svg>
+
+
+                </span>
+
+                <span className="feature-heading__txt">
+                  Independant Sellers
+
+
+                </span>
+
+
+
+              </h3>
+
+
+
+              <div className="feature-txt">
+
+                <span className="feature-txt__txt">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia 
+                  provident fuga veniam autem praesentium repellendus id exercitationem 
+                  dolorem saepe dolores aliquam odio, 
+                </span>
+                
+              </div>
+              
+            </div>
+
+            <div className="feature feature3">
+
+              <h3 className="feature-heading">
+
+                <span className="feature-heading__icon">
+
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><path d="M9.057,20.471L2.293,13.707a1,1,0,0,1,1.414-1.414l5.236,5.236,11.3-13.18a1,1,0,1,1,1.518,1.3Z"></path></svg>
+
+
+                </span>
+
+                <span className="feature-heading__txt">
+                  Secure Transactions
+
+
+                </span>
+
+
+
+              </h3>
+
+
+
+              <div className="feature-txt">
+
+                <span className="feature-txt__txt">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia 
+                  provident fuga veniam autem praesentium repellendus id exercitationem 
+                  dolorem saepe dolores aliquam odio,
+                </span>
+
+                
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+
+
+      </section>
+
+      <section className="popular">
+
+        <div className="popular-heading">
+          <h1>
+            Popular Items
+          </h1>
+        </div>
+
+        <div className="popular-cards">
+
+          <ul className="popular-list">
+
+            <li className="popular-list-item">
+
+              <a href="#">
+
+                <div className="popular-list-item__image">
+                  <img src={woodspoon} alt="wooden spoons"/>
+
+                </div>
+
+                <div className="popular-list-item__info">
+
+                  <span className="title">
+                    Wooden Spoon Set
+                  </span>
+                  <br/>
+                  <span className="vendor">
+                    Wooden Creations
+                  </span>
+                  <br/>
+                  <span className="stars"></span>
+
+
+                </div>
+
+              </a>
+
+            </li>
+
+          </ul>
 
         </div>
 
@@ -179,14 +326,9 @@ function App() {
 
 
 
-
-
-
-
-
-
-
       </section>
+
+
 
 
 
